5c4304245cec100d1b41f4e89bb439414ac5358f
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-04-15 09:41:37 -0500

    fix: Update workflow to include testing of the generated executable and improve launcher.py structure

--- launcher.py
+++ launcher.py
@@ -1,9 +1,16 @@
-import runpy
 import sys
 import os
 import runpy
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+if getattr(sys, 'frozen', False):
+    
+    base_path = sys._MEIPASS
+else:
+    
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+sys.path.insert(0, os.path.join(base_path, "src"))
 
 if __name__ == "__main__":
-    runpy.run_module("src.presentation.main", run_name="__main__")
+    runpy.run_module("presentation.main", run_name="__main__")
