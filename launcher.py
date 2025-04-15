da884b70796e7dbe0c737889866fcb4528d0a221
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-04-15 09:34:15 -0500

    fix: Update module path in launcher.py for correct execution

--- launcher.py
+++ launcher.py
@@ -6,4 +6,4 @@ import runpy
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 if __name__ == "__main__":
-    runpy.run_module("presentation.main", run_name="__main__")
+    runpy.run_module("src.presentation.main", run_name="__main__")
