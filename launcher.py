7c947a7fdc4805215cf77bfb2c7950f524c64c8e
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-04-15 09:25:36 -0500

    fix: Remove duplicate import statements and ensure sys.path is set correctly

--- launcher.py
+++ launcher.py
@@ -1,4 +1,9 @@
 import runpy
+import sys
+import os
+import runpy
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 if __name__ == "__main__":
     runpy.run_module("src.presentation.main", run_name="__main__")
